31c9cf6827bdd281a02f97bf3d6e46a84d026107
prasad51 <mattaprasadg@gmail.com>
2020-11-08 23:37:21 +0530

    edge detection

--- edgeV.py
+++ edgeV.py
@@ -3,8 +3,6 @@ import numpy as np
 
 img = 'test_case1.jpg'
 
-schoass_kernal = [[3,0,-3],[10,0,-10],[3,0,-3]] 
-sobel_kernal = [[1,0,-1],[2,0,-2],[1,0,-1]]
 vertical_kernal = [[1,0,-1],[1,0,-1],[1,0,-1]]
 horizontal_kernal = [[1,1,1],[0,0,0],[-1,-1,-1]]
 
@@ -39,7 +37,7 @@ class EDGE:
         self.gen_img = np.array(self.gen_img,dtype=np.uint8)
         return self.gen_img
 
-edge_vertical = EDGE(img,schoass_kernal)
+edge_vertical = EDGE(img,vertical_kernal)
 vertical_img = edge_vertical.vertical()
 
 edge_horizontal = EDGE(img,horizontal_kernal)
